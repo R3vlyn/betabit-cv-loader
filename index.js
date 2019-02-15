934f6e7f0f7b4378a7bc045ef4a23848c73a2398
Michiel van Eijkeren <m.vaneijkeren@gmail.com>
2019-02-15 10:12:48 +0100

    react front, node back

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const http = require("http");
-const port = process.env.PORT || 3000;
-const handler = (req, res) => {
-    console.log('Server received request.🙂' );
-    res.end('Hello World');
-};
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-const server = http.createServer(handler);
+app.get('/api/customers', (req, res) => {
+    const customers = [{
+        id:1, name: "john doe"
+    },{
+        id:2, name: "henk doe"
+    },{
+        id:3, name: "tito doe"
+    }];
+    res.json(customers);
+})
 
-server.listen(port, err =>{
-    if(err){
-        console.log(err)
-    } else{
-        console.log(`Server listening on port: ${port}`);
-    }
-});
+app.listen(port, () => console.log(`server started on port ${port}`));
+
+// const http = require("http");
+
+// const handler = (req, res) => {
+//     console.log('Server received request.🙂' );
+//     res.end('Hello World');
+// };
+
+// const server = http.createServer(handler);
+
+// server.listen(port, err =>{
+//     if(err){
+//         console.log(err)
+//     } else{
+//         console.log(`Server listening on port: ${port}`);
+//     }
+// });
